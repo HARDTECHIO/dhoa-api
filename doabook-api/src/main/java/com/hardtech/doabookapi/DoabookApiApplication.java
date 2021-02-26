b3c68a0f44d09d6e4643a78248ff5b488b6c4d50
Jecesario <jp@soloid.com.br>
2021-02-26 10:08:28 -0300

    chore(project): criando e configurando projeto

---
+++ doabook-api/src/main/java/com/hardtech/doabookapi/DoabookApiApplication.java
@@ -0,0 +1,13 @@
+package com.hardtech.doabookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoabookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoabookApiApplication.class, args);
+	}
+
+}
