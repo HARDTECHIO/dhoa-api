be814e810720e4120f3cc83843a4f450166fb70f
Lucas-schimit <lucas_schimit@outlook.com>
2021-02-26 14:13:35 -0300

    feat(temas): Criando metodo delete

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/TemasController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/TemasController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,4 +46,8 @@ public class TemasController {
 		return ResponseEntity.ok(repository.save(tema));
 	}
 	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
