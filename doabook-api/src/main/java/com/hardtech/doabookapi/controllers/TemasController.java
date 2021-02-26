ddea4e979221947691b6a6ee150255eec4539625
Lucas-schimit <lucas_schimit@outlook.com>
2021-02-26 11:06:39 -0300

    feat(temas): Criando metodo getAll

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/TemasController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/TemasController.java
@@ -1,10 +1,15 @@
 package com.hardtech.doabookapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hardtech.doabookapi.models.Temas;
 import com.hardtech.doabookapi.repositories.TemasRepository;
 
 @RestController
@@ -14,6 +19,11 @@ public class TemasController {
 
 	@Autowired
 	private TemasRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Temas>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 	
 	
 }
