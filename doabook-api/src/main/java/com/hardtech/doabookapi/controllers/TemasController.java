7c205435c005c6f5ef2f714769aa49b3dda2c66a
Lucas-schimit <lucas_schimit@outlook.com>
2021-02-26 14:09:29 -0300

    feat(temas): Criando metodo put

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/TemasController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/TemasController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,9 @@ public class TemasController {
 		return repository.findById(id).map(obj -> ResponseEntity.ok(obj)).orElse(ResponseEntity.notFound().build());
 	}
 	
+	@PutMapping
+	public ResponseEntity<Temas> put (@RequestBody Temas tema){
+		return ResponseEntity.ok(repository.save(tema));
+	}
 	
 }
