966d52ae7117c6f81a4e0b3a4a91199bf8011148
ShenSouza <ds052798ts@gmail.com>
2021-03-01 12:14:51 -0300

    feat(controller) Criado método Delete #4

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,11 @@ public class PostagensController {
 	@PutMapping
 	public ResponseEntity<Postagens> put(@RequestBody Postagens postagem) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 
 }
