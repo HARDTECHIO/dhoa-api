10afa3fb609d848f85d5ad58dd77a78606859749
ShenSouza <ds052798ts@gmail.com>
2021-03-01 11:59:39 -0300

    feat(controller): Criado método post #4

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
@@ -3,9 +3,12 @@ package com.hardtech.doabookapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,11 @@ public class PostagensController {
 	@GetMapping
 	public ResponseEntity<List<Postagens>> getAll() {
 		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@PostMapping
+	public ResponseEntity<Postagens> post(@RequestBody Postagens postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
 	}
 
 }
