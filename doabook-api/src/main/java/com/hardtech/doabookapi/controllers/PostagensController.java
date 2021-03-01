ffe2e3c80bc837d9851e60f3d4145db53ead0ec0
ShenSouza <ds052798ts@gmail.com>
2021-03-01 12:07:12 -0300

    feat(controller) Criado método getById #4

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,11 @@ public class PostagensController {
 	@GetMapping
 	public ResponseEntity<List<Postagens>> getAll() {
 		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagens> getById(@PathVariable long id) {
+		return repository.findById(id).map(obj -> ResponseEntity.ok(obj)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@PostMapping
