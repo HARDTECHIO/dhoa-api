3112c97afe10e95b0b902d8405c44d5445529264
ShenSouza <ds052798ts@gmail.com>
2021-03-01 11:51:12 -0300

    feat(controller): Criado método getAll #4

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
@@ -1,10 +1,15 @@
 package com.hardtech.doabookapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hardtech.doabookapi.models.Postagens;
 import com.hardtech.doabookapi.repositories.PostagensRepository;
 
 @RestController
@@ -14,5 +19,10 @@ public class PostagensController {
 
 	@Autowired
 	private PostagensRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Postagens>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 
 }
