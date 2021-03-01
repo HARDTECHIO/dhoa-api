e07f032f8cefd5bc1179948d2e86ebabb7e9cf41
ShenSouza <ds052798ts@gmail.com>
2021-03-01 12:19:33 -0300

    feat(repository): Criado Método FindAllByTitulo e FindAllByTexto #2

--- doabook-api/src/main/java/com/hardtech/doabookapi/repositories/PostagensRepository.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/repositories/PostagensRepository.java
@@ -1,9 +1,13 @@
 package com.hardtech.doabookapi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hardtech.doabookapi.models.Postagens;
 
 public interface PostagensRepository extends JpaRepository<Postagens, Long> {
-	
+	public List<Postagens> findAllByTituloContainingIgnoreCase(String titulo);
+
+	public List<Postagens> findAllByTextoContainingIgnoreCase(String texto);
 }
