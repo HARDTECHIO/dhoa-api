49b7ec839ea90a4f020b38358ec78e8ddb75be52
ShenSouza <ds052798ts@gmail.com>
2021-03-01 12:11:28 -0300

    feat(controller): Criado método PUT #4

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/PostagensController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,11 @@ public class PostagensController {
 	@PostMapping
 	public ResponseEntity<Postagens> post(@RequestBody Postagens postagem) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+
+	@PutMapping
+	public ResponseEntity<Postagens> put(@RequestBody Postagens postagem) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
 
 }
