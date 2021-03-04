0d337f65445c202f648b6d9f322b31a06d12a55a
Jecesario <jp@soloid.com.br>
2021-03-04 15:03:03 -0300

    feat(controllers): criado o metodo logar #10

--- doabook-api/src/main/java/com/hardtech/doabookapi/controllers/UsuariosController.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/UsuariosController.java
@@ -3,6 +3,7 @@ package com.hardtech.doabookapi.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hardtech.doabookapi.models.Usuarios;
+import com.hardtech.doabookapi.models.UsuariosLogin;
 import com.hardtech.doabookapi.service.UsuariosService;
 
 @RestController
@@ -30,5 +32,10 @@ public class UsuariosController {
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().build();
 		}
+	}
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UsuariosLogin> logar(@RequestBody Optional<UsuariosLogin> usuario) {
+		return service.logar(usuario).map(obj -> ResponseEntity.ok(obj)).orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 }
