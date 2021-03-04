9e25cecb1e112f1f7a92a4990951376b7bc86762
Jecesario <jp@soloid.com.br>
2021-03-04 11:27:04 -0300

    chore(repositories): criado repository de Usuarios #9

---
+++ doabook-api/src/main/java/com/hardtech/doabookapi/repositories/UsuariosRepository.java
@@ -0,0 +1,9 @@
+package com.hardtech.doabookapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hardtech.doabookapi.models.Usuarios;
+
+public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+	
+}
