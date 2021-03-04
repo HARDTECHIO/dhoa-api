4dc7aca86ffa75532c5e39da8d6d44d7ea4d5c55
Jecesario <jp@soloid.com.br>
2021-03-04 14:41:02 -0300

    feat(controllers): criado o metodo cadastrar #10

---
+++ doabook-api/src/main/java/com/hardtech/doabookapi/controllers/UsuariosController.java
@@ -0,0 +1,34 @@
+package com.hardtech.doabookapi.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hardtech.doabookapi.models.Usuarios;
+import com.hardtech.doabookapi.service.UsuariosService;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping(value = "/usuarios")
+public class UsuariosController {
+
+	@Autowired
+	private UsuariosService service;
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuarios> cadastrar(@RequestBody Usuarios usuario) {
+		Optional<Usuarios> user = service.cadastrar(usuario);
+
+		try {
+			return ResponseEntity.ok(user.get());
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+}
