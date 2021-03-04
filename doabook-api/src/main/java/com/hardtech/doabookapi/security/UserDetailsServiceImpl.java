ffe5b208ecb30ad13f52cf014160958415e1e8eb
Jecesario <jp@soloid.com.br>
2021-03-04 14:06:26 -0300

    chore(security): criado classe UserDatailsServiceImpl #7

---
+++ doabook-api/src/main/java/com/hardtech/doabookapi/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.hardtech.doabookapi.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.hardtech.doabookapi.models.Usuarios;
+import com.hardtech.doabookapi.repositories.UsuariosRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuariosRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Usuarios> user = repository.findByUsuario(username);
+		user.orElseThrow(() -> new UsernameNotFoundException(username + " not found."));
+		return user.map(UserDetailsImpl::new).get();
+	}
+
+}
