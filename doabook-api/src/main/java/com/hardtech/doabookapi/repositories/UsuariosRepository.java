87a79bb4aa66082013cbe953ac72d6e4e1f9889f
Jecesario <jp@soloid.com.br>
2021-03-04 14:00:21 -0300

    feat(repository): adicionado metodo findByUsuario #9

--- doabook-api/src/main/java/com/hardtech/doabookapi/repositories/UsuariosRepository.java
+++ doabook-api/src/main/java/com/hardtech/doabookapi/repositories/UsuariosRepository.java
@@ -1,9 +1,11 @@
 package com.hardtech.doabookapi.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hardtech.doabookapi.models.Usuarios;
 
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
-	
+	Optional<Usuarios> findByUsuario(String usuario);
 }
